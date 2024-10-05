1cc21d2bd7db5b669f8118dc7446e3d35002c7a1
Yusuf <111415081+yusuf-bot@users.noreply.github.com>
2024-10-05 22:05:33 +0400

    Add files via upload

---
+++ coloriser.py
@@ -0,0 +1,308 @@
+import tensorflow as tf
+import keras 
+from keras import layers
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+from tqdm import tqdm
+import re
+from keras.preprocessing.image import img_to_array
+
+# to get the files in proper order
+def sorted_alphanumeric(data):  
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)',key)]
+    return sorted(data,key = alphanum_key)
+
+
+
+
+# defining the size of the image
+SIZE =  256
+color_img = []
+path = 'Landscape/color'
+files = os.listdir(path)
+files = sorted_alphanumeric(files)
+for i in tqdm(files):    
+        if i == '4200.jpg':
+            break
+        else:    
+            img = cv2.imread(path + '/'+i,1)
+            # open cv reads images in BGR format so we have to convert it to RGB
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            #resizing image
+            img = cv2.resize(img, (SIZE, SIZE))
+            img = img.astype('float32') / 255.0
+            color_img.append(img_to_array(img))
+
+
+gray_img = []
+path = 'Landscape/gray'
+files = os.listdir(path)
+files = sorted_alphanumeric(files)
+for i in tqdm(files):
+         if i == '4200.jpg':
+            break
+         else: 
+            img = cv2.imread(path + '/'+i,1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            #resizing image
+            img = cv2.resize(img, (SIZE, SIZE))
+            img = img.astype('float32') / 255.0
+            gray_img.append(img_to_array(img))
+
+color_dataset=tf.data.Dataset.from_tensor_slices(np.array(color_img[:4000])).batch(64)
+gray_dataset=tf.data.Dataset.from_tensor_slices(np.array(gray_img[:4000])).batch(64)
+
+color_dataset_t=tf.data.Dataset.from_tensor_slices(np.array(color_img[4000:])).batch(8)
+gray_dataset_t=tf.data.Dataset.from_tensor_slices(np.array(gray_img[4000:])).batch(8)
+Color_test = color_img[4000:]
+Gray_test = gray_img[4000:]
+print("Number of batches:", color_dataset.cardinality().numpy())
+for batch in color_dataset.take(1):
+    print("Shape of one batch:", batch.shape)
+
+
+def downsample(filters, size, apply_batchnorm=True):
+  
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                             kernel_initializer='he_normal', use_bias=False))
+
+  if apply_batchnorm:
+    result.add(tf.keras.layers.BatchNormalization())
+
+  result.add(tf.keras.layers.LeakyReLU())
+
+  return result
+
+def upsample(filters, size, apply_dropout=False):
+  
+
+  result = tf.keras.Sequential()
+  result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer='he_normal',
+                                    use_bias=False))
+
+  result.add(tf.keras.layers.BatchNormalization())
+
+  if apply_dropout:
+      result.add(tf.keras.layers.Dropout(0.5))
+
+  result.add(tf.keras.layers.ReLU())
+
+  return result  
+
+def Generator():
+  inputs = tf.keras.layers.Input(shape=[256,256,3])
+
+  down_stack = [
+    downsample(64, 4, apply_batchnorm=False), # (bs, 128, 128, 64)
+    downsample(128, 4), # (bs, 64, 64, 128)
+    downsample(256, 4), # (bs, 32, 32, 256)
+    downsample(512, 4), # (bs, 16, 16, 512)
+    downsample(512, 4), # (bs, 8, 8, 512)
+    downsample(512, 4), # (bs, 4, 4, 512)
+    downsample(512, 4), # (bs, 2, 2, 512)
+    downsample(512, 4), # (bs, 1, 1, 512)
+  ]
+
+  up_stack = [
+    upsample(512, 4, apply_dropout=True), # (bs, 2, 2, 1024)
+    upsample(512, 4, apply_dropout=True), # (bs, 4, 4, 1024)
+    upsample(512, 4, apply_dropout=True), # (bs, 8, 8, 1024)
+    upsample(512, 4), # (bs, 16, 16, 1024)
+    upsample(256, 4), # (bs, 32, 32, 512)
+    upsample(128, 4), # (bs, 64, 64, 256)
+    upsample(64, 4), # (bs, 128, 128, 128)
+  ]
+
+  initializer = tf.random_normal_initializer(0., 0.02)
+  last = tf.keras.layers.Conv2DTranspose(3, 4,
+                                         strides=2,
+                                         padding='same',
+                                         kernel_initializer=initializer,
+                                         activation='tanh') # (bs, 256, 256, 3)
+
+  x = inputs
+
+  # Downsampling through the model
+  skips = []
+  for down in down_stack:
+    x = down(x)
+    skips.append(x)
+
+  skips = reversed(skips[:-1])
+
+  # Upsampling and establishing the skip connections
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    x = tf.keras.layers.Concatenate()([x, skip])
+
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+
+def Discriminator():
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  inp = tf.keras.layers.Input(shape=[SIZE, SIZE, 3], name='input_image')
+  tar = tf.keras.layers.Input(shape=[SIZE, SIZE, 3], name='target_image')
+
+  x = tf.keras.layers.concatenate([inp, tar]) # (bs, 256, 256, channels*2)
+
+  down1 = downsample(64, 4, False)(x) # (bs, 128, 128, 64)
+  down2 = downsample(128, 4)(down1) # (bs, 64, 64, 128)
+  down3 = downsample(256, 4)(down2) # (bs, 32, 32, 256)
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3) # (bs, 34, 34, 256)
+  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                kernel_initializer=initializer,
+                                use_bias=False)(zero_pad1) # (bs, 31, 31, 512)
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu) # (bs, 33, 33, 512)
+
+  last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                kernel_initializer=initializer)(zero_pad2) # (bs, 30, 30, 1)
+
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+
+
+generator = Generator()
+generator.summary()
+
+discriminator = Discriminator()
+discriminator.summary()
+
+
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+generator_optimizer = tf.keras.optimizers.Adam( beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam( beta_1=0.5)
+
+LAMBDA = 100
+
+def generator_loss(disc_generated_output, gen_output, target):
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+  # mean absolute error
+  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+  return total_gen_loss, gan_loss, l1_loss
+
+
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+
+  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss
+def train_step(input_image, target, epoch):
+  print('a')
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    gen_output = generator(input_image, training=True)
+    print('aa')
+    disc_real_output = discriminator([input_image, target], training=True)
+    print('aaa')
+    disc_generated_output = discriminator([input_image, gen_output], training=True)
+    print('aaaa')
+
+    gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+    print('aaaaa')
+    disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+    print('aaaaaa')
+  generator_gradients = gen_tape.gradient(gen_total_loss,
+                                          generator.trainable_variables)
+  print('aaaaaaa')
+  discriminator_gradients = disc_tape.gradient(disc_loss,
+                                               discriminator.trainable_variables)
+  print('aaaaaaaa7')
+  generator_optimizer.apply_gradients(zip(generator_gradients,
+                                          generator.trainable_variables))
+  print('a8')
+  discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                              discriminator.trainable_variables))
+  print('a9')
+ 
+import time
+def fit(train_ds, epochs,):
+  for epoch in range(epochs):
+    start = time.time()
+
+    print("Epoch: ", epoch+1)
+
+    # Train
+    for n, (input_image, target) in train_ds.enumerate():
+      
+      train_step(input_image, target, epoch)
+      print('.')
+    print()
+
+  
+
+    print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
+                                                        time.time()-start))
+  
+def generate_images(model, test_input, tar):
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15,15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+  for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # getting the pixel values between [0, 1] to plot it.
+    plt.imshow(display_list[i])
+    plt.axis('off')
+  plt.show()
+
+for example_input, example_target in tf.data.Dataset.zip((gray_dataset,color_dataset)).take(2):
+  generate_images(generator, example_input, example_target)
+
+fit(tf.data.Dataset.zip((gray_dataset, color_dataset)),
+    epochs = 50)
+
+
+generator.compile(loss='MSE', optimizer="adam", metrics=['accuracy'])
+for example_input, example_target in tf.data.Dataset.zip((gray_dataset_t,color_dataset_t)).take(11):
+    Gray_test = example_input
+    Color_test = example_target
+
+
+ModelLoss, ModelAccuracy = generator.evaluate( Gray_test, Color_test)
+
+print('Test Loss is {}'.format(ModelLoss))
+print('Test Accuracy is {}'.format(ModelAccuracy ))
+
+def generate_images(model, test_input, tar):
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15,15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+  for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # getting the pixel values between [0, 1] to plot it.
+    plt.imshow(display_list[i])
+    plt.axis('off')
+  plt.show()
+
+for example_input, example_target in tf.data.Dataset.zip((gray_dataset_t,color_dataset_t)).take(30):
+  generate_images(generator, example_input, example_target)
